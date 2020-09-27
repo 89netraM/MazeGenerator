4c06237134f4662eb27a63832da7f16c12289d7d
Mårten Åsberg <marten.asberg@outlook.com>
2020-09-27 10:40:40 +0200

    Maps `rows` and `columns` are now public
    And swapped the order for the constructors so that the match with the other functions.

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,19 @@ fn main() {
 	stdout
 		.execute(cursor::Hide)
 		.expect("Could not hide cursor.");
-	let map = Map::generate_with_peek(30, 15, (0, 0), &mut move |map| {
+	let map = Map::generate_with_peek(15, 30, (0, 0), &mut move |map| {
 		if move_height > 0 {
 			stdout
 				.execute(cursor::MoveUp(move_height))
 				.expect("Could not move cursor.");
 		}
 		writeln!(stdout, "{}", map).expect("Could not write.");
-		move_height = 16;
+		move_height = (map.rows as u16) + 1;
 
 		thread::sleep(Duration::from_millis(50));
 	});
 
-	if let Some(path) = map.solve((0, 0), (14, 29)) {
+	if let Some(path) = map.solve((0, 0), (map.rows - 1, map.columns - 1)) {
 		println!(
 			"Path: {}",
 			path.into_iter()
--- src/map.rs
+++ src/map.rs
@@ -145,80 +145,79 @@ const DIRECTIONS: [Direction; 4] = [
 ];
 
 pub struct Map {
-	width: usize,
-	height: usize,
+	pub rows: usize,
+	pub columns: usize,
 	map: Vec<Vec<bool>>,
 }
 
 impl Map {
-	pub fn new(width: usize, height: usize) -> Map {
-		let mut map = Vec::with_capacity(height * 2 - 1);
+	pub fn new(rows: usize, columns: usize) -> Map {
+		let mut map = Vec::with_capacity(rows * 2 - 1);
 
-		for r in 0..(height * 2 - 1) {
+		for r in 0..(rows * 2 - 1) {
 			if r % 2 == 0 {
-				map.push(vec![true; width - 1]);
+				map.push(vec![true; columns - 1]);
 			} else {
-				map.push(vec![true; width]);
+				map.push(vec![true; columns]);
 			}
 		}
 
-		Map { width, height, map }
+		Map { rows, columns, map }
 	}
 
-	pub fn generate(width: usize, height: usize, start: (usize, usize)) -> Map {
-		Map::generate_with_peek(width, height, start, &mut |_| {})
+	pub fn generate(rows: usize, columns: usize, start: (usize, usize)) -> Map {
+		Map::generate_with_peek(rows, columns, start, &mut |_| {})
 	}
 	pub fn generate_with_peek<F>(
-		width: usize,
-		height: usize,
+		rows: usize, columns: usize,
 		start: (usize, usize),
 		peek: &mut F,
 	) -> Map
 	where
 		F: FnMut(&Map),
 	{
-		let mut map = Map::new(width, height);
+		let mut map = Map::new(rows, columns);
 		map.make_path_with_peek(start, &mut HashSet::new(), &mut thread_rng(), peek);
 		map
 	}
 
 	pub fn set_above(&mut self, r: usize, c: usize, closed: bool) {
-		assert!(0 < r && r < self.height && c < self.width);
+		assert!(0 < r && r < self.rows && c < self.columns);
 
 		self.map[(r * 2) - 1][c] = closed;
 	}
 	pub fn is_above(&self, r: usize, c: usize) -> bool {
-		assert!(0 < r && r < self.height && c < self.width);
+		assert!(0 < r && r < self.rows && c < self.columns);
 
 		self.map[(r * 2) - 1][c]
 	}
 	pub fn set_left(&mut self, r: usize, c: usize, closed: bool) {
-		assert!(r < self.height && 0 < c && c < self.width);
+		assert!(r < self.rows && 0 < c && c < self.columns);
 
 		self.map[r * 2][c - 1] = closed;
 	}
 	pub fn is_left(&self, r: usize, c: usize) -> bool {
-		assert!(r < self.height && 0 < c && c < self.width);
+		assert!(r < self.rows && 0 < c && c < self.columns);
 
 		self.map[r * 2][c - 1]
 	}
 	pub fn set_right(&mut self, r: usize, c: usize, closed: bool) {
-		assert!(r < self.height && c < self.width - 1);
+		assert!(r < self.rows && c < self.columns - 1);
 
 		self.map[r * 2][c] = closed;
 	}
 	pub fn is_right(&self, r: usize, c: usize) -> bool {
-		assert!(r < self.height && c < self.width - 1);
+		assert!(r < self.rows && c < self.columns - 1);
 
 		self.map[r * 2][c]
 	}
 	pub fn set_below(&mut self, r: usize, c: usize, closed: bool) {
-		assert!(r < self.height - 1 && c < self.width);
+		assert!(r < self.rows - 1 && c < self.columns);
 
 		self.map[(r * 2) + 1][c] = closed;
 	}
 	pub fn is_below(&self, r: usize, c: usize) -> bool {
-		assert!(r < self.height - 1 && c < self.width);
+		assert!(r < self.rows - 1 && c < self.columns);
 
 		self.map[(r * 2) + 1][c]
 	}
@@ -233,10 +232,10 @@ impl Map {
 	}
 	pub fn is(&self, r: usize, c: usize, dir: &Direction) -> bool {
 		match dir {
-			Direction::Up if 0 < r && r < self.height && c < self.width => self.is_above(r, c),
-			Direction::Left if r < self.height && 0 < c && c < self.width => self.is_left(r, c),
-			Direction::Right if r < self.height && c < self.width - 1 => self.is_right(r, c),
-			Direction::Down if r < self.height - 1 && c < self.width => self.is_below(r, c),
+			Direction::Up if 0 < r && r < self.rows && c < self.columns => self.is_above(r, c),
+			Direction::Left if r < self.rows && 0 < c && c < self.columns => self.is_left(r, c),
+			Direction::Right if r < self.rows && c < self.columns - 1 => self.is_right(r, c),
+			Direction::Down if r < self.rows - 1 && c < self.columns => self.is_below(r, c),
 			_ => true,
 		}
 	}
@@ -249,8 +248,8 @@ impl Map {
 		match dir {
 			Direction::Up if current.0 > 0 => Some((current.0 - 1, current.1)),
 			Direction::Left if current.1 > 0 => Some((current.0, current.1 - 1)),
-			Direction::Right if current.1 < self.width - 1 => Some((current.0, current.1 + 1)),
-			Direction::Down if current.0 < self.height - 1 => Some((current.0 + 1, current.1)),
+			Direction::Right if current.1 < self.columns - 1 => Some((current.0, current.1 + 1)),
+			Direction::Down if current.0 < self.rows - 1 => Some((current.0 + 1, current.1)),
 			_ => None,
 		}
 	}
@@ -268,8 +267,8 @@ impl Map {
 	}
 
 	pub fn solve(&self, from: (usize, usize), to: (usize, usize)) -> Option<Vec<Direction>> {
-		assert!(from.0 < self.height && from.1 < self.width);
-		assert!(to.0 < self.height && to.1 < self.width);
+		assert!(from.0 < self.rows && from.1 < self.columns);
+		assert!(to.0 < self.rows && to.1 < self.columns);
 
 		if from == to {
 			return Some(Vec::new());
@@ -349,24 +348,24 @@ fn build_path(
 impl fmt::Display for Map {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		let mut above;
-		let mut below = Vec::with_capacity(self.width + 1);
+		let mut below = Vec::with_capacity(self.columns + 1);
 
 		below.push(UPPER_LEFT);
-		below.append(&mut vec![HORIZONTAL; self.width - 1]);
+		below.append(&mut vec![HORIZONTAL; self.columns - 1]);
 		below.push(UPPER_RIGHT);
 
-		for r in 0..(self.height - 1) {
+		for r in 0..(self.rows - 1) {
 			above = below;
-			below = vec![WallJunction::new(); self.width + 1];
+			below = vec![WallJunction::new(); self.columns + 1];
 			below[0] = VERTICAL;
-			below[self.width] = VERTICAL;
+			below[self.columns] = VERTICAL;
 
-			for c in 0..(self.width - 1) {
+			for c in 0..(self.columns - 1) {
 				above[c + 1].set_down(self.map[r * 2][c]);
 				below[c + 1].set_up(self.map[r * 2][c]);
 			}
 
-			for c in 0..self.width {
+			for c in 0..self.columns {
 				below[c].set_right(self.map[r * 2 + 1][c]);
 				below[c + 1].set_left(self.map[r * 2 + 1][c]);
 			}
@@ -382,12 +381,12 @@ impl fmt::Display for Map {
 		}
 
 		above = below;
-		below = vec![HORIZONTAL; self.width + 1];
+		below = vec![HORIZONTAL; self.columns + 1];
 		below[0] = LOWER_LEFT;
-		below[self.width] = LOWER_RIGHT;
-		for c in 0..(self.width - 1) {
-			above[c + 1].set_down(self.map[self.height * 2 - 2][c]);
-			below[c + 1].set_up(self.map[self.height * 2 - 2][c]);
+		below[self.columns] = LOWER_RIGHT;
+		for c in 0..(self.columns - 1) {
+			above[c + 1].set_down(self.map[self.rows * 2 - 2][c]);
+			below[c + 1].set_up(self.map[self.rows * 2 - 2][c]);
 		}
 
 		writeln!(
