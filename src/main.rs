132551c58d36be119522228a7ab9f1eb7da8dfda
Mårten Åsberg <marten.asberg@outlook.com>
2020-09-27 12:19:57 +0200

    Accepting arguments

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,66 @@ extern crate crossterm;
 use crossterm::{cursor, ExecutableCommand};
 use std::io::{stdout, Write};
 
+extern crate clap;
+use clap::{App, Arg};
+use std::str::FromStr;
+
 use std::{thread, time::Duration};
 
 mod map;
 use map::Map;
 
 fn main() {
+	let matches = App::new("Maze Generator")
+		.arg(Arg::with_name("ROWS")
+			.long("rows")
+			.default_value("5")
+			.validator(|s| usize::from_str(&s).map(|_| ()).or(Err("Must be a number".to_string())))
+			.help("Number of rows of the generated map")
+			.display_order(0),
+		)
+		.arg(Arg::with_name("COLUMNS")
+			.long("columns")
+			.default_value("5")
+			.validator(|s| usize::from_str(&s).map(|_| ()).or(Err("Must be a number".to_string())))
+			.help("Number of columns of the generated map")
+			.display_order(1),
+		)
+		.arg(Arg::with_name("START_ROW")
+			.long("start_row")
+			.default_value("0")
+			.validator(|s| usize::from_str(&s).map(|_| ()).or(Err("Must be a number".to_string())))
+			.help("The row to start generating from")
+			.display_order(2),
+		)
+		.arg(Arg::with_name("START_COLUMN")
+			.long("start_column")
+			.default_value("0")
+			.validator(|s| usize::from_str(&s).map(|_| ()).or(Err("Must be a number".to_string())))
+			.help("The column to start generating from")
+			.display_order(3),
+		)
+		.arg(Arg::with_name("DELAY")
+			.long("delay")
+			.default_value("50")
+			.validator(|s| usize::from_str(&s).map(|_| ()).or(Err("Must be a number".to_string())))
+			.help("The ms delay between steps")
+			.display_order(4),
+		)
+		.get_matches();
+
+	let rows = matches.value_of("ROWS").map(usize::from_str).unwrap().unwrap();
+	let columns = matches.value_of("COLUMNS").map(usize::from_str).unwrap().unwrap();
+	let start_row = matches.value_of("START_ROW").map(usize::from_str).unwrap().unwrap();
+	let start_column = matches.value_of("START_COLUMN").map(usize::from_str).unwrap().unwrap();
+	let delay = matches.value_of("DELAY").map(u64::from_str).unwrap().unwrap();
+
 	let mut stdout = stdout();
 	let mut move_height = 0;
 	stdout
 		.execute(cursor::Hide)
 		.expect("Could not hide cursor.");
-	let map = Map::generate_with_peek(15, 30, (0, 0), &mut move |map| {
+	let map = Map::generate_with_peek(rows, columns, (start_row, start_column), &mut |map| {
 		if move_height > 0 {
 			stdout
 				.execute(cursor::MoveUp(move_height))
@@ -22,8 +70,13 @@ fn main() {
 		writeln!(stdout, "{}", map).expect("Could not write.");
 		move_height = (map.rows as u16) + 1;
 
-		thread::sleep(Duration::from_millis(50));
+		if delay > 0 {
+			thread::sleep(Duration::from_millis(delay));
+		}
 	});
+	stdout
+		.execute(cursor::Show)
+		.expect("Could not show cursor.");
 
 	if let Some(path) = map.solve((0, 0), (map.rows - 1, map.columns - 1)) {
 		println!(
